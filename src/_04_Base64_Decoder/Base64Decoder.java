6666b7935e40707ce45e833cd5c245f71866608f
League Student <league@iMac-29.attlocal.net>
2019-12-02 20:02:16 -0800

    base64 to byte array

--- src/_04_Base64_Decoder/Base64Decoder.java
+++ src/_04_Base64_Decoder/Base64Decoder.java
@@ -56,46 +56,107 @@ public class Base64Decoder {
 	public static byte[] convert4CharsTo24Bits(String s){
 		
 		
-		System.out.println(s);
+		//System.out.println(s);
 		
 		int[] input = new int[4];
 		byte[] output = new byte[3];
-		System.out.println("test1");
+		//System.out.println("test1");
 		
 		for (int j = 0; j < s.length(); j++) {
-			System.out.println("characterConvertedToBase64   "+convertBase64Char(s.charAt(j)));
-			System.out.println("numAddedToByte  "+Integer.parseInt(printBinary(convertBase64Char(s.charAt(j)))));
+			//System.out.println("characterConvertedToBase64   "+convertBase64Char(s.charAt(j)));
+			//System.out.println("numAddedToByte  "+Integer.parseInt(printBinary(convertBase64Char(s.charAt(j)))));
 			input[j] = Integer.parseInt(printBinary(convertBase64Char(s.charAt(j))));
 		}
 		
-		System.out.println("test2");
+		//System.out.println("test2");
 		
 		double arrayOfValues = 0;
 		
 		for (int i = 0; i < input.length; i++) {
-			System.out.println(input[i]);
-			System.out.println(arrayOfValues);
-			System.out.println("power     "+pow(10, 6));
+			//System.out.println(input[i]);
+			//System.out.println(arrayOfValues);
+			//System.out.println("power     "+pow(10, 6));
 			arrayOfValues = ((arrayOfValues)*pow(10, 6))+input[i];
-			System.out.println("array   "+arrayOfValues);
+			//System.out.println("array   "+arrayOfValues);
+			
 		}
 		
+		System.out.println("full input:  " + input[0] + " " + input[1] + " " + input[2] + " " + input[3]);
 		
-		for (int i = 0; i < output.length; i++) {
-			output[i] = (byte) (arrayOfValues % pow(10, (3-i)*8));
-			System.out.println("value  "+pow(10, (2-i)*8));
-			System.out.println("value1  "+(arrayOfValues % pow(10, (3-i)*8)));
-			System.out.println("value2  "+output[i]);
-		}
-		System.out.println("test3");
+			int a = ((input[0]*100) + (input[1]/10000));
+			//System.out.println("input 1 " + input[1]);
+			//System.out.println("input 1 convert " + input[1]/10000);
+			//System.out.println("output 1:  " + a);
+			System.out.println("0     " + input[2] + "    " + input[3]);
+			System.out.println("1     " + ((input[2]%100))*1000000);
+			System.out.println("2     " + (input[3]));
+			int b = (((input[1]%10000))*10000) + ((input[2])/100);
+			System.out.println("output 2:  " + b);
+			System.out.println((input[2] - ((input[2]%10000)))*100);
+			int c = (((input[2]%100))*1000000) + (input[3]);
+			System.out.println("output 3:  " + c);
+		
+			output[0] = (byte) (printBaseTen(a));
+			output[1] = (byte) (printBaseTen(b));
+			output[2] = (byte) (printBaseTen(c));
+			
+			for (int i = 0; i < output.length; i++) {
+				System.out.println();
+				System.out.println(output[i]);
+			}
+			
 		return output;
 	}
 	
-	static double pow (double a, double b) {
-		 if ( b == 0)        return 1;
-		    if ( b == 1)        return a;
-		    if (b%2 == 0)    return     pow ( a * a, b/2); //even a=(a^2)^b/2
-		    else                return a * pow ( a * a, b/2); //odd  a=a*(a^2)^b/2
+	private static int printBaseTen(int a) {
+		double output = 0;
+		String binaryString= a + "";
+		int decimal=Integer.parseInt(binaryString,2);
+		System.out.println(decimal);
+		output = decimal;
+		/*int currentvalue = 1;
+		System.out.println("a = " + a);
+		int b = a/10000000;
+		System.out.println("1st Value = " + b);
+		int c = (a-(b*1000000))/1000000;
+		System.out.println("2nd Value = " + c);
+		int d = (a-b-(c*100000))/100000;
+		System.out.println("3rd Value = " + d);
+		int e = (a-b-c-d)/10000;
+		System.out.println("4th Value = " + e);
+		int f = (a-b-c-d-e)/1000;
+		System.out.println("5th Value = " + f);
+		int g = (a-b-c-d-e-f)/100;
+		System.out.println("6th Value = " + g);
+		int h = (a-b-c-d-e-f-g)/10;
+		System.out.println("7th Value = " + h);
+		int i = (a-b-c-d-e-f-g-h);
+		System.out.println("8th Value = " + i);
+		
+		int[] input = {b, c, d, e, f, g, h, i};
+		
+		for (int j = 0; j < input.length; j++) {
+			
+			System.out.println((input[7-j] * currentvalue));
+			output += (input[7-j] * currentvalue);
+			System.out.println("current output: " + output);
+			
+			currentvalue *= 2;
+			a /= 10;
+			
+			//System.out.println("current Value  " + currentvalue);
+			
+		}
+		
+		System.out.println("Output  " + output);
+		*/return (int)(output);
+	}
+
+	static double pow (long d, long e) {
+		 if ( e == 0)        return 1;
+		    if ( e == 1)        return d;
+		    if (e%2 == 0)    return     pow ( d * d, e/2); //even a=(a^2)^b/2
+		    else                return d * pow ( d * d, e/2); //odd  a=a*(a^2)^b/2
 	}
 	
 	public static String printBinary(long b) {
@@ -109,6 +170,9 @@ public class Base64Decoder {
 			}
 			else num = "0" + num;
 			b = (long) (b >> 1);
+		}
+		while(num.length()<6) {
+			num = "0" + num;
 		}
 			System.out.println("OutputFromPrintByBinary   "+num);
 			return num;
